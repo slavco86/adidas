8462b75ce6b7db5ffe28a96f70228728613a0af4
slavco zute <slav.zute@jdplc.com>
2018-07-02 17:05:06 +0100

    add Spot and correct Linting

---
+++ src/components/Spot.vue
@@ -0,0 +1,46 @@
+<template>
+  <a
+    :href="url"
+    class="ga-ip spot">
+    <picture v-if="image">
+      <source
+        v-if="responsiveImg"
+        :srcset="image.mobile"
+        media="(max-width: 975px)">
+      <img
+        :src="image.desktop"
+        alt="">
+    </picture>
+    <slot/>
+  </a>
+</template>
+
+<script>
+export default {
+  name: 'Spot',
+  props: {
+    image: {
+      type: Object,
+      default() {
+        return {};
+      },
+    },
+    url: {
+      type: String,
+      default() {
+        return '';
+      },
+    },
+  },
+
+  computed: {
+    responsiveImg() {
+      if (this.image && Object.prototype.hasOwnProperty.call(this.image, 'mobile') && Object.prototype.hasOwnProperty.call(this.image, 'desktop')) {
+        return true;
+      }
+
+      return false;
+    },
+  },
+};
+</script>
