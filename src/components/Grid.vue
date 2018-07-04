f6ff01daa1897aa2cdfb80d7615c3506feffa450
Cedric Siewe <cedric.siewe@jdplc.com>
2018-07-04 13:36:45 +0100

    Added products animation

--- src/components/Grid.vue
+++ src/components/Grid.vue
@@ -1,38 +1,89 @@
 <template>
-  <div class="grid">
+  <div class="grid" :class="{ 'grid__product--animate' : activateAnimation }">
 
-    <a href="#">
+    <a href="#"
+      class="grid__product">
 
-      <img src="http://via.placeholder.com/620x620"
+      <img src="http://via.placeholder.com/350x350"
         alt="">
     </a>
-    <a href="#">
-      <img src="http://via.placeholder.com/620x620"
+
+    <a href="#"
+      class="grid__product">
+      <img src="http://via.placeholder.com/350x350"
         alt="">
     </a>
-    <a href="#">
-      <img src="http://via.placeholder.com/620x620"
+
+    <a href="#" class="grid__product">
+      <img src="http://via.placeholder.com/350x350"
         alt="">
     </a>
-    <a href="#">
-      <img src="http://via.placeholder.com/620x620"
+
+    <a href="#" class="grid__product">
+      <img src="http://via.placeholder.com/350x350"
         alt="">
     </a>
   </div>
 </template>
 
+<script>
+export default {
+  name: 'Grid',
+  props: {
+
+  },
+
+  data() {
+    return {
+      activateAnimation: false
+    }
+  }
+}
+</script>
+
 <style lang="scss" scoped>
   .grid {
     display: flex;
-    // flex: 0 0 50%;
     flex-flow: row wrap;
-    max-width: 1240px;
     justify-content: center;
-    margin: 0 auto;
-    background: hotpink;
+    margin: 1rem auto;
+    overflow: hidden;
 
-    a {
-      // padding: 0.25rem;
+    &__product {
+      flex: 0 0 50%;
+      height: auto;
+      max-width: 635px;
+      min-width: 150px;
+      padding: 0.5rem;
+      transition: transform 0.7s;
+
+      &:first-child {
+        transform: translate3d(-600px, -600px, 0);
+      }
+
+      &:nth-child(2) {
+        transform: translate3d(600px, -600px, 0);
+      }
+
+      &:nth-child(3) {
+        transform: translate3d(-600px, 600px, 0);
+      }
+
+      &:last-child {
+        transform: translate3d(600px, 600px, 0);
+      }
+
+      &--animate {
+        .grid__product {
+          transform: translate3d(0, 0, 0);
+        }
+      }
+
+      img {
+        display: block;
+        height: auto;
+        width: 100%;
+      }
     }
   }
 </style>
