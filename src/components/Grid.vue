e9045c2449056f33cde57fe00f4d16edfeb163ec
Cedric Siewe <cedric.siewe@jdplc.com>
2018-07-04 12:12:12 +0100

    Created social file and added in router

---
+++ src/components/Grid.vue
@@ -0,0 +1,39 @@
+<template>
+  <div class="grid">
+
+    <a href="#">
+
+      <img src="http://via.placeholder.com/620x620"
+        alt="">
+    </a>
+    <a href="#">
+      <img src="http://via.placeholder.com/620x620"
+        alt="">
+    </a>
+    <a href="#">
+      <img src="http://via.placeholder.com/620x620"
+        alt="">
+    </a>
+    <a href="#">
+      <img src="http://via.placeholder.com/620x620"
+        alt="">
+    </a>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+  .grid {
+    display: flex;
+    // flex: 0 0 50%;
+    flex-flow: row wrap;
+    max-width: 1240px;
+    justify-content: center;
+    margin: 0 auto;
+    background: hotpink;
+
+    a {
+      // padding: 0.25rem;
+    }
+  }
+</style>
+
---
+++ src/views/Social.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="social">
+    <p>Get Social</p>
+    <h2>#Creator</h2>
+    <Grid/>
+  </div>
+</template>
+
+<script>
+import Grid from '@/components/Grid.vue';
+
+export default {
+  name: 'Spocial',
+  components: {
+    Grid
+  }
+
+}
+</script>
+
