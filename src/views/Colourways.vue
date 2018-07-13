37e35618dd369a31c641afce71aa0cfc03234713
Ali Kayani <Ali.Kayani@jdplc.com>
2018-07-13 17:01:34 +0100

    added colourways compoenent

--- src/views/Colourways.vue
+++ src/views/Colourways.vue
@@ -1,40 +1,50 @@
-// Written by Ali K last modified 11/07/2018
+// Written by Ali K last modified 13/07/2018
 <template>
   <div class="colourwayscontainer">
+    <!-- This is the main container which contains the adidas logo, close button (called error),
+    the two plus buttons -->
     <div class="mainimage">
-      <img class="error"       src="../assets/close_white.png">
+      <img class="close"       src="../assets/close_white.png">
       <img class="adidas"      src="../assets/adidas_white.svg">
       <img class="add"         src="../assets/add.png">
-      <img class="mainshoe"    :src="mainImage">
-      <h1 class="derupt">Derupt</h1>
+      <!-- the main shoe image is given a dynamic html value based on what image the user clicks
+      in the carousel -->
+      <img class="mainshoe"   :src="defaultImage"> 
       <img class="addafter"    src="../assets/add.png">
-    </div>
-    <div class="shoppingbag">
-      <h3 class="shoppingbagtext">Derupt</h3>
+      <!-- This is the shopping  items which consist of the title of the shoe, price and bag picture -->
+      <h3 class="shoppingbagtexttitle">DERUPT</h3>
       <h4 class="shoppingbagtext">£80.00</h4>
-      <br />
-       <img src="../assets/shoppingbag.png">
+      <img src="../assets/shoppingbag.png"  class="shoppingbagicon">
     </div>
-    <Carousel :slides="slides" :options="options" class="carousel-container">
+    <!-- The carousel container shows different show images. On click will change the main show image -->
+    <!-- The line below is the main container which sets up the slides and passes in the options -->
+    <Carousel :slides="slides" :options="options" ref="carousel" class="carousel-container">
+      <!-- The image container appears on the line below -->
       <div class="image-container" slot-scope="{slide}">
-        <img :src="slide.image" class="image" @click="testMethod(slide.image)"/>
+        <!-- On click the slide url is passed to the click event handler -->
+        <img :src="slide.image" class="image" @click="mainImage($refs.carousel.$children[0].$children[0].swiper.clickedIndex)"/>
       </div>
     </Carousel>
+    <!-- Derupt title -->
+    <h1  class="derupt">Derupt</h1>
   </div>
 </template>
 
 <script>
-  import Carousel from '@/containers/Carousel.vue';
+  import Carousel from '@/containers/Carousel.vue'; //imports the casrousel component (part of swiper)
 
+  //exports the carousel component and gives it a name for future use.
   export default {
   name: 'Colourways-container',
   components: {
     Carousel,
   },
 
+  //the date method sets up the vue component. First it declares the main image variable which will change when the user clicks it.
+  //the slides array contains all the images. They are hosted at http://assets.jdsports.com/
   data() {
     return {
-      mainImage: "https://jdsports-client-resources.co.uk/jdsports-client-resources/img/2018/0712/shoe-desktop-1-6e0103d3b8a1653704b09acad135b6a9.png",
+      defaultImage: '',
       slides: [
         {
           image: "https://jdsports-client-resources.co.uk/jdsports-client-resources/img/2018/0712/shoe-desktop-0-04670ce96db4cc1da203cc6895aa85e0.png"
@@ -58,16 +68,33 @@
           image:"https://jdsports-client-resources.co.uk/jdsports-client-resources/img/2018/0712/shoe-desktop-6-2001fef6fd965b9b7ff7456d1f103818.png"
         },
         ],
+
+        //The options are found at http://idangero.us/swiper/api/. They are swiper options which are fed into the carousel.
       options: {
         slidesPerView: 6,
-        speed: 400,
-        spaceBetween: -550
+        breakpoints: {
+          376: {
+            slidesPerView: 4,
+            spaceBetween: 10,
+          }
         }
       }
-    },
+    }
+  },
+  mounted() {
+    this.mainImage(0);
+  },
+  // computed: {
+  //       defaultImage() {
+  //         return this.slides[0].image
+  //       }
+  //     },
+  //This method receives a click event when the user clicks on an image in the carousel. It is then assigned to the
+  //main image variable which will change the main shoe display image.
   methods:{
-    testMethod(event) {
-      this.mainImage=event
+  mainImage(index) {
+      this.defaultImage =  this.slides[index].image;
+      console.log(this.$refs);
       }
   }
 }
@@ -76,8 +103,182 @@
 
 <style lang="scss" scoped>
 
+// The fonts below are the Gotham font used by JD. They are stored in the assets folder.
+// They are differnent weights.
 
-  .colourwayscontainer {
+@font-face {
+font-family: 'HCo Gotham Cond SSm';
+src:url('../assets/fonts/B1CFB00FA67031B66.woff2') format('woff2'),	
+url('../assets/fonts/B1CFB00FA67031B66.woff') format('woff');
+font-weight: 400;
+font-style: normal;
+}
+
+
+@font-face {
+font-family: 'HCo Gotham Cond SSm';
+src:url('../assets/fonts/9AF882D5BD620EB9E.woff2') format('woff2'),
+url('../assets/fonts/9AF882D5BD620EB9E.woff') format('woff');
+font-weight: 500;
+font-style: normal;
+}
+
+
+@font-face {
+font-family: 'HCo Gotham Cond SSm';
+src:url('../assets/fonts/DD21C5786A9E30A24.woff2') format('woff2'),	
+url('../assets/fonts/DD21C5786A9E30A24.woff') format('woff');
+font-weight: 600;
+font-style: normal;
+}
+
+
+@font-face {
+font-family: 'HCo Gotham SSm';
+src:url('../assets/fonts/F4715F49B60D9B144.woff2') format('woff2'),
+url('../assets/fonts/F4715F49B60D9B144.woff') format('woff');
+font-weight: 400;
+font-style: normal;
+}
+
+
+@font-face {
+font-family: 'HCo Gotham SSm';
+src:url('../assets/fonts/725262BC71949F842.woff2') format('woff2'),
+url('../assets/fonts/725262BC71949F842.woff') format('woff');
+font-weight: 500;
+font-style: normal;
+}
+
+
+@font-face {
+font-family: 'HCo Gotham SSm';
+src:url('../assets/fonts/B5416F0FED9EA9CD2.woff2') format('woff2'),
+url('../assets/fonts/B5416F0FED9EA9CD2.woff') format('woff');
+font-weight: 700;
+font-style: normal;
+}
+
+// --------------------------Start of the main styling. Mobile first----------------------------------------
+
+//sets the maincontainer to the design back colour and sets the height.
+.colourwayscontainer {
+     background: #222222;
+     height: 95vh;
+    //  width:100%;
+  }
+
+//hides the adidas logo as it's not needed on the mobile view
+.adidas {
+  display: none;
+}
+
+//sets the mainiamge container to a relative position as the child components will be positioned by using
+//the position property
+.mainimage {
+    position: relative;
+    z-index: 1;
+  }
+
+//this styles the close button. Essentially positioning in near the top right hand corner and setting its height.
+.close {
+  position: absolute;
+  top: 2%;
+  right: 4%;
+  height: 42px;
+}
+
+.derupt {
+  position: absolute;
+  top: 207px;
+  left: -236px;
+  font-family: 'HCo Gotham SSm';
+  transform: rotate(270deg);
+  font-size: 49vw;
+  font-weight: 900;
+  z-index: 0;
+  color:#333333;
+  letter-spacing: -10px;
+
+}
+
+//this styles the plus icon above and to the left of the shoe
+.add {
+    position: absolute;
+    top: 36%;
+    left: 19%;
+    width: 4.5%;
+    height: 4%;
+}
+
+//this styles the main show placeholder which is dynamicallt changed
+//when the user click on an image on the carousel
+.mainshoe {
+  margin-top: 30%;
+  width: 85%;
+  height: 85%;
+}
+
+//this styles the right plus icon 
+.addafter {
+  position: absolute;
+  top: 76%;
+  right: 39%;
+  width: 4.5%;
+  height: 4%;
+}
+
+//this styles the shopping bag icon
+
+.shoppingbagicon {
+  position: absolute;
+  width: 50px;
+  top: 62%;
+  right: 6%;
+}
+
+//this styles the Derupt title
+.shoppingbagtexttitle {
+  position: absolute;
+  top: 50%;
+  right: 6%;
+  color: white;
+  font-size: 11px;
+}
+
+//this styles the price
+.shoppingbagtext {
+  position: absolute;
+  top: 54%;
+  right: 6%;
+  color: white;
+  font-size: 10px;
+}
+
+.carousel-container {
+  margin-top: 15%;
+}
+
+.image-container {
+  position: relative;
+}
+
+.image {
+  display: block;
+  width: 80px;
+  height: 74px;
+  opacity: 0.5;
+}
+
+.image:hover {
+  opacity: 1;
+}
+
+//----------------------------------//Media query for larger screens------------------------------------------
+
+@media only screen and (min-width: 766px) {
+
+.colourwayscontainer {
      background: #222222;
      height: 100vh;
   }
@@ -93,10 +294,9 @@
     width: 14%;
     top: 0%;
     left: 4%;
-    colour: white;
 }
 
-  .error {
+  .close {
     position: absolute;
     top: 4%;
     height: 10%;
@@ -120,7 +320,7 @@
 
 .addafter {
     position: absolute;
-    top:   74%;
+    top:   44%;
     left: 58%;
     width: 2%;
     height: 5%;
@@ -174,6 +374,11 @@
 .image:hover {
   opacity: 1;
 }
+
+}
+
+
+  
 
 </style>
 
