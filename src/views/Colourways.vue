cdc955ab0ad2c8d705753f060c4acf1388984a04
Cedric Siewe <cedric.siewe@jdplc.com>
2018-07-13 11:04:30 +0100

    Got the anmiation working the way we wanted

--- src/views/Colourways.vue
+++ src/views/Colourways.vue
@@ -1,96 +1,63 @@
 <template>
-  <div
-    id="skrollr-body"
-    class="colourways">
-    <!-- Dummy Text -->
-    <div class="dummy-text">
-      <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Eos odio, quia in eveniet
-      ducimus tempore alias sequi molestiae animi nobis ullam debitis consectetur iure! Sapiente
-      tenetur voluptas neque nobis numquam architecto quaerat aliquid, veritatis illum doloremque
-      vitae explicabo. Sed, qui.</p>
-      <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Eos odio, quia in eveniet
-      ducimus tempore alias sequi molestiae animi nobis ullam debitis consectetur iure! Sapiente
-      tenetur voluptas neque nobis numquam architecto quaerat aliquid, veritatis illum doloremque
-      vitae explicabo. Sed, qui.</p>
-      <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Eos odio, quia in eveniet
-      ducimus tempore alias sequi molestiae animi nobis ullam debitis consectetur iure! Sapiente
-      tenetur voluptas neque nobis numquam architecto quaerat aliquid, veritatis illum doloremque
-      vitae explicabo. Sed, qui.</p>
-      <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Eos odio, quia in eveniet
-      ducimus tempore alias sequi molestiae animi nobis ullam debitis consectetur iure! Sapiente
-      tenetur voluptas neque nobis numquam architecto quaerat aliquid, veritatis illum doloremque
-      vitae explicabo. Sed, qui.</p>
-      <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Eos odio, quia in eveniet
-      ducimus tempore alias sequi molestiae animi nobis ullam debitis consectetur iure! Sapiente
-      tenetur voluptas neque nobis numquam architecto quaerat aliquid, veritatis illum doloremque
-      vitae explicabo. Sed, qui.</p>
-    </div>
-    <!-- Big Image / Info -->
-    <div class="colourways__info">
-      <!-- Big-image -->
-      <span
-        class="colourways__info__img-holder"
-        data-bottom-top="transform:translate3d(0, 100%, 0); opacity:0;"
-        data--500-bottom="transform:translate3d(0, 0, 0); opacity: 1;">
-        <img
-          src="//via.placeholder.com/350x350"
-          alt="big-image">
-      </span>
-      <!-- Text -->
-      <div class="colourways__info__text">
-        <h4>Deerupt</h4>
-        <p>£55.99</p>
-        <div class="quick-buy"/>
+  <div class="container">
+    <!-- First Content -->
+    <div class="content">
+      <!-- Content Title -->
+      <h1 class="content__title">360 Degree</h1>
+      <!-- Content Overlay -->
+      <div
+        class="content__overlay"
+        data-start="height: 0;"
+        data-970-end="height: 100%;">
+        <!-- Overlay Title -->
+        <div class="content__overlay__info">
+          <!-- Image Container -->
+          <span class="content__overlay__img-holder">
+            <!-- <div class="content__overlay__more-info">+</div> -->
+            <img
+              src="//cdn.zeplin.io/5b3f79354781e8b30dae1eba/screens/cache/41200c67601c2624efeb330326af2ded700d1e43.png"
+              alt="adidas-trainers">
+              <!-- <div class="content__overlay__more-info">+</div> -->
+          </span>
+          <!-- Content Text -->
+          <div class="content__overlay__text">
+            <h4>Deerupt</h4>
+            <p>£80.00</p>
+            <span class="quick-buy"/>
+          </div>
+        </div>
+        <!-- Colourways List -->
+        <ul
+          class="content__overlay__list"
+          data-500-start="bottom: 2.55rem;"
+          data-600-end="bottom: 1rem;">
+          <li>
+            <img
+              src="//via.placeholder.com/70x70"
+              alt="small-image">
+          </li>
+          <li>
+            <img
+              src="//via.placeholder.com/70x70"
+              alt="small-image"></li>
+          <li>
+            <img
+              src="//via.placeholder.com/70x70"
+              alt="small-image">
+          </li>
+          <li>
+            <img
+              src="//via.placeholder.com/70x70"
+              alt="small-image">
+          </li>
+        </ul>
       </div>
-      <!-- List -->
-      <ul
-        class="colourways__info__list"
-        data-bottom-top="transform:translate3d(0, 100%, 0)"
-        data--800-bottom="transform:translate3d(0, 0, 0)">
-        <li>
-          <img
-            src="//via.placeholder.com/70x70"
-            alt="colourways">
-        </li>
-        <li>
-          <img
-            src="//via.placeholder.com/70x70"
-            alt="colourways">
-        </li>
-        <li>
-          <img
-            src="//via.placeholder.com/70x70"
-            alt="colourways">
-        </li>
-        <li>
-          <img
-            src="//via.placeholder.com/70x70"
-            alt="colourways">
-        </li>
-      </ul>
     </div>
-    <!-- Dummy Text -->
-    <div class="dummy-text">
-      <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Eos odio, quia in eveniet
-      ducimus tempore alias sequi molestiae animi nobis ullam debitis consectetur iure! Sapiente
-      tenetur voluptas neque nobis numquam architecto quaerat aliquid, veritatis illum doloremque
-      vitae explicabo. Sed, qui.</p>
-      <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Eos odio, quia in eveniet
-      ducimus tempore alias sequi molestiae animi nobis ullam debitis consectetur iure! Sapiente
-      tenetur voluptas neque nobis numquam architecto quaerat aliquid, veritatis illum doloremque
-      vitae explicabo. Sed, qui.</p>
-      <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Eos odio, quia in eveniet
-      ducimus tempore alias sequi molestiae animi nobis ullam debitis consectetur iure! Sapiente
-      tenetur voluptas neque nobis numquam architecto quaerat aliquid, veritatis illum doloremque
-      vitae explicabo. Sed, qui.</p>
-      <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Eos odio, quia in eveniet
-      ducimus tempore alias sequi molestiae animi nobis ullam debitis consectetur iure! Sapiente
-      tenetur voluptas neque nobis numquam architecto quaerat aliquid, veritatis illum doloremque
-      vitae explicabo. Sed, qui.</p>
-      <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Eos odio, quia in eveniet
-      ducimus tempore alias sequi molestiae animi nobis ullam debitis consectetur iure! Sapiente
-      tenetur voluptas neque nobis numquam architecto quaerat aliquid, veritatis illum doloremque
-      vitae explicabo. Sed, qui.</p>
+
+    <!-- Second Content -->
+    <div class="content">
+      <!-- Content Title -->
+      <h1 class="content__title">Other Content</h1>
     </div>
   </div>
 </template>
@@ -109,94 +76,126 @@ export default {
 </script>
 
 <style lang="scss" scoped>
-  .colourways {
+  .container {
     background-color: #333;
     color: #fff;
-    display: flex;
-    flex-direction: column;
-    line-height: 1.5;
     margin: 0 auto;
-    max-width: 1250px;
-    padding: 1rem;
+    max-width: 1950px;
+    overflow-x: hidden;
+    overflow-y: auto;
+    scroll-padding: 100px 0 0;
+    scroll-snap-type: y proximity;
+  }
 
-    &__info {
-      // background: hotpink;
+  .content {
+    align-items: center;
+    display: flex;
+    height: 100vh;
+    justify-content: center;
+    overflow: hidden;
+    position: relative;
+    scroll-snap-align: start none;
+    width: 100%;
+
+    &__title {
+      font-size: 3rem;
+
+      &:first-child {
+        color: #fff;
+      }
+    }
+
+    &:first-child {
+      background-color: yellowgreen;
+    }
+
+    &:last-child {
+      background-color: aquamarine;
+
+      .content__title {
+        color: #333;
+      }
+    }
+
+    // Overlay
+    &__overlay {
+      align-items: center;
+      background-color: #333;
+      bottom: 0;
       display: flex;
       flex-direction: column;
-      max-width: 750px;
-      overflow: hidden;
-      position: relative;
-
-      &__img-holder {
-        width: 100%;
-        display: block;
-        padding-bottom: 100px;
-
-        img {
-          display: block;
-          width: 100%;
-          height: auto;
-        }
-      }
-
-      &__text {
-        position: absolute;
-        right: 0;
-        top: 50%;
-        // margin-top: -50px;
-        margin-right: 1rem;
-
-        .quick-buy {
-          width: 30px;
-          height: 30px;
-          position: relative;
-          left: 50%;
-          margin-left: -10px;
-          top: 0.5rem;
-          border: 2px solid orangered;
-          border-radius: 50%;
-          transition: all 0.2s ease-in-out;
-
-          &:hover {
-            background-color: orangered;
-          }
-        }
-
-        h4 {
-          font-size: 1rem;
-        }
-
-        p {
-          font-size: 0.8rem;
-          margin-top: 0.5rem;
-          margin-bottom: 0;
-        }
-      }
+      justify-content: flex-end;
+      left: 0;
+      position: absolute;
+      width: 100%;
+      transition: all 1.5s linear;
+      z-index: 1;
 
       &__list {
         list-style: none;
-        display: flex;
         padding: 0;
-        margin-top: 0.5rem;
+        margin: 3rem 0;
+        position: relative;
+        display: flex;
         margin-bottom: 0;
-        width: 100%;
-        align-items: center;
-        flex-flow: row wrap;
-        justify-content: flex-start;
-        transition: transform 0.5ms linear;
-        position: absolute;
-        bottom: 0;
+        transition: all 0.5s linear;
 
         li {
           margin: 0 0.2rem;
           flex: 0 0 23%;
         }
       }
-    }
-  }
 
-  .dummy-text p {
-    font-size: 1rem;
-    line-height: 1.5;
+      &__info {
+        display: flex;
+        max-height: 750px;
+        justify-content: center;
+      }
+
+      &__img-holder {
+        max-width: 500px;
+        position: relative;
+
+        img {
+          display: block;
+          height: auto;
+          width: 100%;
+        }
+      }
+
+      &__text {
+        position: absolute;
+        top: 60%;
+        right: 35%;
+        margin: 0 1rem;
+
+        h4 {
+          font-size: 1rem;
+        }
+
+        p {
+          margin: 0;
+        }
+      }
+
+      &__more-info {
+        color: grey;
+        cursor: pointer;
+        font-size: 3rem;
+        position: relative;
+        z-index: 2;
+
+        &:first-child {
+          top: 50%;
+          margin-top: -50px;
+          left: 5%;
+        }
+
+        &:last-child {
+          bottom: 5%;
+          left: 55%;
+        }
+      }
+    }
   }
 </style>
