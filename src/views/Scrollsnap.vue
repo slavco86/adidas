94980781f7f84f41ed813523fdd2d517cd4545a8
Cedric Siewe <cedric.siewe@jdplc.com>
2018-07-17 13:09:04 +0100

    Added the scrollsnap animation

--- src/views/Scrollsnap.vue
+++ src/views/Scrollsnap.vue
@@ -1,31 +1,84 @@
 <template>
-  <div class="scroll-snap">
-    <div class="content">
-      <h1>Scroll Snap</h1>
-    </div>
-    <div class="content">
-      <h1>Scroll Snap 2</h1>
-    </div>
-    <div class="content">
-      <h1>Scroll Snap 3</h1>
+  <div id="container">
+    <section>
+      <h1>First</h1>
+    </section>
+    <section>
+      <h1>Second</h1>
+    </section>
+    <section>
+      <h1>Third</h1>
+    </section>
+
+    <div class="status" v-cloak>
+      Active panel: {{activePanelName}}
     </div>
   </div>
 </template>
 
+<script>
+import PanelSnap from 'panelsnap';
+
+export default {
+  data() {
+    return {
+      activePanel: null,
+    };
+  },
+
+  computed: {
+    activePanelName() {
+      if (!this.activePanel) {
+        return '';
+      }
+      return this.activePanel.querySelector('h1').innerText;
+    },
+  },
+
+  mounted() {
+    this.panelSnapInstance = new PanelSnap({
+      panelSelector: '#container > section',
+    });
+
+    this.panelSnapInstance.on('activatePanel', this.activatePanel);
+  },
+
+  methods: {
+    activatePanel(panel) {
+      this.activePanel = panel;
+    }
+  },
+};
+</script>
+
 <style lang="scss" scoped>
-  .content {
-    height: 100vh;
-    background-color: aquamarine;
+  section {
+    min-height: calc(100vh + 20px);
+    background-color: cyan;
+    color: #333;
     display: flex;
     justify-content: center;
     align-items: center;
+    font-size: 3rem;
 
     &:nth-child(2) {
       background-color: hotpink;
     }
 
     &:last-child {
-      background-color: yellowgreen;
+      background-color: greenyellow;
     }
+  }
+
+  .status {
+    position: fixed;
+    bottom: 0;
+    left: 0;
+    right: 0;
+    padding: 10px;
+    text-align: center;
+  }
+  [v-cloak] {
+    visibility: hidden;
   }
 </style>
