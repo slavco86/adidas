20111d4e93c431c7b92f7186f34f3799bcb4bdc2
Stephen Riley <stephen.riley2@jdplc.com>
2018-07-18 09:40:39 +0100

    remove unrequired routes

--- src/router.js
+++ src/router.js
@@ -1,8 +1,6 @@
 import Vue from 'vue';
 import Router from 'vue-router';
 import Home from './views/Home.vue';
-import About from './views/About.vue';
-import Social from './views/Social.vue';
 
 Vue.use(Router);
 
@@ -16,18 +14,6 @@ export default new Router({
       component: Home,
     },
     {
-      path: '/about',
-      name: 'about',
-      component: About,
-    },
-    {
-      path: '/social',
-      name: 'social',
-      component: Social,
-    },
-    {
-      // Last modified by Ali Kayani on 11/07/2017
-      // Enables the Colourways route to be loaded
       path: '/:gender/:franchise',
       name: 'colourways',
       component: Content,
