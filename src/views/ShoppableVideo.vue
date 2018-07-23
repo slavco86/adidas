a3d1d8ad187b34169568b26945e9fadeeca57115
Stephen Riley <ste@steriley.com>
2018-07-23 12:09:08 +0100

    make iframes fill screen

--- src/views/ShoppableVideo.vue
+++ src/views/ShoppableVideo.vue
@@ -1,11 +1,11 @@
 <template>
-  <div class="c-container">
-    <h2>Shoppable Video</h2>
-    <iframe
-      :src="src"
-      allowfullscreen
-      frameborder="0"/>
-    <a :href="cta.href">{{ cta.text }}</a>
+  <div class="content-slide">
+    <div class="content-slide__inner">
+      <iframe
+        :src="src"
+        allowfullscreen
+        frameborder="0"/>
+    </div>
   </div>
 </template>
 
@@ -16,22 +16,27 @@ export default {
       type: String,
       default: 'https://ssl-dot-player-dot-smartzer-editor.appspot.com/video?video=jpNti3sr7TwPxohDN3tdiVZR&companyId=58331cf19f43139bd4702949'
     },
-    cta: {
-      type: Object,
-      default: () => ({
-        href: '/campaign/Summer+Advert/?facet-campaign=summer-advert',
-        text: 'SHOP THE SUMMER ADVERT',
-      }),
-    },
   },
 };
 </script>
 
 <style scoped>
 iframe {
+  width: 100%;
+  height: 90vh;
+}
+
+.content-slide {
+  align-items: center;
+  display: flex;
   height: 100vh;
-  max-height: 500px;
-  min-height: 400px;
+  justify-content: center;
+}
+
+.content-slide__inner {
+  display: flex;
+  flex-direction: column;
+  padding: 1rem;
   width: 100%;
 }
 </style>
--- src/views/SketchFabModel.vue
+++ src/views/SketchFabModel.vue
@@ -1,13 +1,15 @@
 <template>
-  <div class="sketchfab-embed-wrapper">
-    <iframe
-      :src="src"
-      width="640"
-      height="480"
-      frameborder="0"
-      allow="autoplay; fullscreen; vr"
-      mozallowfullscreen="true"
-      webkitallowfullscreen="true"/>
+  <div class="content-slide">
+    <div class="content-slide__inner">
+      <iframe
+        :src="src"
+        width="640"
+        height="480"
+        frameborder="0"
+        allow="autoplay; fullscreen; vr"
+        mozallowfullscreen="true"
+        webkitallowfullscreen="true"/>
+    </div>
   </div>
 </template>
 
@@ -23,5 +25,22 @@ export default {
 </script>
 
 <style>
+iframe {
+  width: 100%;
+  height: 90vh;
+}
 
+.content-slide {
+  align-items: center;
+  display: flex;
+  height: 100vh;
+  justify-content: center;
+}
+
+.content-slide__inner {
+  display: flex;
+  flex-direction: column;
+  padding: 1rem;
+  width: 100%;
+}
 </style>
