6bea22ae064300e521b2221c0ee70d033bcfd71f
Stephen Riley <stephen.riley2@jdplc.com>
2018-07-25 12:19:50 +0100

    css linting added to lint command

--- src/components/Modal.vue
+++ src/components/Modal.vue
@@ -91,37 +91,36 @@ export default {
   .modal-container {
     width: 765px;
     height: 1024px;
-    margin: 0px auto;
+    margin: 0 auto;
     padding: 20px 30px;
     background-color: #fff;
     border-radius: 2px;
-    box-shadow: 0 2px 8px rgba(0, 0, 0, .33);
-    transition: all .3s ease;
+    box-shadow: 0 2px 8px rgba(0, 0, 0, 0.33);
+    transition: all 0.3s ease;
     background-image: url("../assets/swtablet.png");
     background-repeat: no-repeat center center fixed;
     background-size: cover;
-}
-
-#modalh {
-  padding-top: 20%;
-}
-
-@media only screen and (min-width: 1025px) {
-
-.modal-container {
-    width: 718px;
-    height: 529px;
-    margin: 0px auto;
-    padding: 20px 30px;
-    background-color: #fff;
-    border-radius: 2px;
-    box-shadow: 0 2px 8px rgba(0, 0, 0, .33);
-    transition: all .3s ease;
   }
 
   #modalh {
-    padding-top: 8%;
+    padding-top: 20%;
   }
+
+  @media only screen and (min-width: 1025px) {
+    .modal-container {
+      width: 718px;
+      height: 529px;
+      margin: 0 auto;
+      padding: 20px 30px;
+      background-color: #fff;
+      border-radius: 2px;
+      box-shadow: 0 2px 8px rgba(0, 0, 0, 0.33);
+      transition: all 0.3s ease;
+    }
+
+    #modalh {
+      padding-top: 8%;
+    }
 
   }
 
