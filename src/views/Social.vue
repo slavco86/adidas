d3b75a86d7cb44be7c99e8ded3921b8f63c14cc0
slavco zute <slav.zute@jdplc.com>
2018-07-30 14:10:58 +0100

    conditional rendering on quickbuy due to stock may not be available

--- src/views/Social.vue
+++ src/views/Social.vue
@@ -13,6 +13,7 @@
           v-bind="slide"
           class="grid__product">
           <QuickBuy
+            v-if="slide.url"
             :url="slide.url"/>
           <span class="grid__product__text">{{ slide.handle }}</span>
         </Spot>
