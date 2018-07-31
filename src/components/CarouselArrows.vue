c6ccc3945dd47d82d5e7fbb916409278dedec881
Cedric Siewe <cedric.siewe@jdplc.com>
2018-07-31 16:35:18 +0100

    Further amends made on chevron arrows

--- src/components/CarouselArrows.vue
+++ src/components/CarouselArrows.vue
@@ -1,9 +1,13 @@
 <template>
   <!-- Custom Arrows -->
   <div class="chevron-arrows">
-    <div class="chevron-arrow-left"/>
+    <div class="chevron-arrow-wrap">
+      <div class="chevron-arrow--left"/>
+    </div>
     <slot/>
-    <div class="chevron-arrow-right"/>
+    <div class="chevron-arrow-wrap">
+      <div class="chevron-arrow--right"/>
+    </div>
   </div>
 </template>
 
@@ -21,11 +25,10 @@
   z-index: 9999;
 }
 
-.chevron-arrow-left,
-.chevron-arrow-right {
+.chevron-arrow--left,
+.chevron-arrow--right {
   border-bottom: 2px solid rgba(#fff, 0.6);
   border-right: 2px solid rgba(#fff, 0.6);
-  cursor: pointer;
   display: inline-block;
   height: 15px;
   margin: 0 2rem;
@@ -33,33 +36,43 @@
   width: 15px;
 }
 
-.chevron-arrow-left {
+.chevron-arrow--left {
   transform: rotate(-225deg);
-
-  &:hover {
-    border-color: #fff;
-  }
 }
 
-.chevron-arrow-right {
+.chevron-arrow--right {
+  right: 0;
   transform: rotate(-45deg);
-
-  &:hover {
-    border-color: #fff;
-  }
 }
 
 // Women
-.category--women .chevron-arrow-left,
-.category--women .chevron-arrow-right {
+.category--women .chevron-arrow--left,
+.category--women .chevron-arrow--right {
   border-color: rgba(#222, 0.6);
 }
 
-.category--women .chevron-arrow-left:hover {
+.category--women .chevron-arrow-wrap:hover .chevron-arrow--left {
   border-color: #222;
 }
 
-.category--women .chevron-arrow-right:hover {
+.category--women .chevron-arrow-wrap:hover .chevron-arrow--right {
   border-color: #222;
+}
+
+.chevron-arrow-wrap {
+  width: 78px;
+  height: 78px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  cursor: pointer;
+
+  &:hover .chevron-arrow--left {
+    border-color: #fff;
+  }
+
+  &:hover .chevron-arrow--right {
+    border-color: #fff;
+  }
 }
 </style>
