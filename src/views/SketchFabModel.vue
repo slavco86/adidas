0dd0d5bc32634763b2ac31819f7be17b922018cd
slavco zute <slav.zute@jdplc.com>
2018-08-03 13:20:50 +0100

    remove sketchfab avatar

--- src/views/SketchFabModel.vue
+++ src/views/SketchFabModel.vue
@@ -115,7 +115,7 @@ export default {
     },
 
     modelSrc() {
-      return `https://sketchfab.com/models/${this.modelId}/embed?autospin=0.2&amp;autostart=1&amp;camera=0&amp;preload=1&amp;transparent=1`;
+      return `https://sketchfab.com/models/${this.modelId}/embed?autospin=0.2&amp;autostart=1&amp;camera=0&amp;preload=1&amp;transparent=1;ui_infos=0`;
     },
 
     logoColor() {
