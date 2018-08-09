df1996abcf48dfcbbe0024a852a70a33b91a13e2
Stephen Riley <stephen.riley2@jdplc.com>
2018-08-09 14:48:03 +0100

    shift content to public folder

--- src/router.js
+++ src/router.js
@@ -3,24 +3,22 @@ import Router from 'vue-router';
 import SketchFabModel from '@/views/SketchFabModel.vue';
 import Modal from '@/components/Modal.vue';
 import Home from './views/Home.vue';
-import Men from './content/men.json';
-import Women from './content/women.json';
 
 Vue.use(Router);
 
 const Content = () => import(/* webpackChunkName: "page-content" */ './views/Content.vue');
 
-function homeContent(route) {
-  return route.path === '/men' ? { franchises: Men } : { franchises: Women };
-}
+// function homeContent(route) {
+//   return route.path === '/men' ? { franchises: Men } : { franchises: Women };
+// }
 
-const content = { men: Men, women: Women };
+// const content = { men: Men, women: Women };
 
-function franchiseContent(route) {
-  const { gender, franchise } = route.params;
+// function franchiseContent(route) {
+//   const { gender, franchise } = route.params;
 
-  return { content: content[gender].filter(obj => obj.franchise === franchise).pop().content };
-}
+//   return { content: content[gender].filter(obj => obj.franchise === franchise).pop().content };
+// }
 
 export default new Router({
   routes: [
@@ -29,47 +27,22 @@ export default new Router({
       redirect: '/men',
     },
     {
-      path: '/modal',
-      name: 'modal',
-      component: Modal,
-      props: {
-        display: true,
-        background: '#f00',
-        textColor: '#fff',
-        content: [
-          {
-            component: 'titles',
-            content: {
-              headline: 'Hello World',
-              subtext: 'lorems dfglkjdfhg\n\nlksdjfgh lkdsjfgh lsdkfgh ldfkjgh lksdfjgh kjdfgh',
-            },
-          },
-          {
-            component: 'Picture',
-            content: {
-              image: '/test.jpg',
-            },
-          },
-        ],
-      },
-    },
-    {
       path: '/:gender',
       name: 'home',
       component: Home,
-      props: homeContent,
+      // props: homeContent,
     },
     {
       path: '/:gender/:franchise',
       name: 'content',
       component: Content,
-      props: franchiseContent,
+      // props: franchiseContent,
     },
     {
       path: '/:gender/:franchise/3d-view',
       name: '3d-view',
       component: SketchFabModel,
-      props: franchiseContent,
+      // props: franchiseContent,
     },
   ],
 });
