4b9fe768977f94b0655ca57c293a654a2aa2233c
slavco zute <slav.zute@jdplc.com>
2018-08-13 13:12:03 +0100

    reword app.spec tests

--- tests/unit/App.spec.js
+++ tests/unit/App.spec.js
@@ -33,7 +33,116 @@ describe('App', () => {
     });
   });
 
-  it('franchiseContent should return correct data depending on route params', () => {
+  it('request content based on route.gender', () => {
+    const wrapper = factory();
+
+    expect(wrapper.vm.contentPath('women')).toBe('/content/women.json');
+  });
+
+  it('request a content file when component has mounted', () => {
+    const getJSON = jest.fn();
+
+    const $route = {
+      params: {
+        gender: 'men',
+      },
+    };
+
+    shallowMount(Component, {
+      mocks: {
+        $route,
+      },
+      stubs: ['router-view'],
+      methods: { getJSON },
+    });
+
+    expect(getJSON).toHaveBeenCalledTimes(1);
+    expect(getJSON).toHaveBeenCalledWith('/content/men.json', 'men');
+  });
+
+  it('request content on route change', () => {
+    const getJSON = jest.fn();
+
+    const $route = {
+      params: {
+        gender: 'men',
+      },
+    };
+
+    const wrapper = shallowMount(Component, {
+      mocks: {
+        $route,
+      },
+      methods: { getJSON },
+      stubs: ['router-view'],
+    });
+
+    wrapper.vm.$route.params.gender = 'women';
+
+    expect(getJSON).toHaveBeenCalledTimes(2);
+  });
+
+  it('prevent making a network request once a gender key has content', () => {
+    const $route = {
+      params: {
+        gender: 'men',
+        franchise: null,
+      },
+    };
+
+    shallowMount(Component, {
+      mocks: {
+        $route,
+      },
+      stubs: ['router-view'],
+      data: () => ({
+        content: {
+          men: {
+            content: 'test',
+          },
+          women: [],
+        },
+      }),
+    });
+
+    expect(fetch).toHaveBeenCalledTimes(0);
+  });
+
+  it('data will be fetched from server and assigned to a gender key in content', async () => {
+    const $route = {
+      params: {
+        gender: 'women',
+      },
+    };
+
+    const wrapper = shallowMount(Component, {
+      mocks: {
+        $route,
+      },
+      stubs: ['router-view'],
+    });
+
+    global.fetch = mockFetch({
+      content: 'test',
+    });
+
+    const result = await wrapper.vm.getJSON('foo', 'women');
+
+    expect(fetch).toHaveBeenCalledTimes(1);
+
+    expect(result).toEqual({
+      content: 'test',
+    });
+
+    expect(wrapper.vm.content).toEqual({
+      men: [],
+      women: {
+        content: 'test',
+      },
+    });
+  });
+
+  it('content will be filtered based on the franchise url parameter', () => {
     const wrapper = factory();
 
     const route = {
@@ -79,90 +188,7 @@ describe('App', () => {
     });
   });
 
-  it('getJSON should fetch content and apply to correct gender key in vm.content', async () => {
-    const $route = {
-      params: {
-        gender: 'women',
-      },
-    };
-
-    const wrapper = shallowMount(Component, {
-      mocks: {
-        $route,
-      },
-      stubs: ['router-view'],
-    });
-
-    global.fetch = mockFetch({
-      content: 'test',
-    });
-
-    const result = await wrapper.vm.getJSON('foo', 'women');
-
-    expect(fetch).toHaveBeenCalledTimes(1);
-
-    expect(result).toEqual({
-      content: 'test',
-    });
-
-    expect(wrapper.vm.content).toEqual({
-      men: [],
-      women: {
-        content: 'test',
-      },
-    });
-  });
-
-  it('should call getJSON on route change', () => {
-    const getJSON = jest.fn();
-
-    const $route = {
-      params: {
-        gender: 'men',
-      },
-    };
-
-    const wrapper = shallowMount(Component, {
-      mocks: {
-        $route,
-      },
-      methods: { getJSON },
-      stubs: ['router-view'],
-    });
-
-    wrapper.vm.$route.params.gender = 'women';
-
-    expect(getJSON).toHaveBeenCalledTimes(2);
-  });
-
-  it('should call getJson on mounted with correct path', () => {
-    const getJSON = jest.fn();
-
-    const $route = {
-      params: {
-        gender: 'men',
-      },
-    };
-
-    shallowMount(Component, {
-      mocks: {
-        $route,
-      },
-      stubs: ['router-view'],
-      methods: { getJSON },
-    });
-
-    expect(getJSON).toHaveBeenCalledTimes(1);
-    expect(getJSON).toHaveBeenCalledWith('/content/men.json', 'men');
-  });
-
-  it('return correct path based on route params', () => {
-    const wrapper = factory();
-
-    expect(wrapper.vm.contentPath('women')).toBe('/content/women.json');
-  });
-
-  it('it should filter/serve content based on route params', () => {
+  it('filter content based on url change in route.franchise', () => {
     const franchise = jest.fn();
 
     const $route = {
@@ -196,31 +222,4 @@ describe('App', () => {
 
     expect(franchise).toHaveBeenCalledTimes(1);
   });
-
-  it('should not make a network request once a gender key is not an empty array', () => {
-    const $route = {
-      params: {
-        gender: 'men',
-        franchise: null,
-      },
-    };
-
-    shallowMount(Component, {
-      mocks: {
-        $route,
-      },
-      stubs: ['router-view'],
-      data: () => ({
-        content: {
-          men: {
-            content: 'test',
-          },
-          women: [],
-        },
-      }),
-    });
-
-    expect(fetch).toHaveBeenCalledTimes(0);
-  });
-
 });
