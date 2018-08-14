c91ea32c0f06ffcab24619daf2c3dc721f445479
slavco zute <slav.zute@jdplc.com>
2018-08-14 14:55:19 +0100

    start auto populating fields

--- src/views/ContentEditor.vue
+++ src/views/ContentEditor.vue
@@ -1,5 +1,19 @@
 <template>
   <div>
+    <form
+      v-if="json.influencer"
+      class="form">
+      <div
+        v-for="(value, field) in json.influencer.products[0]"
+        :key="field"
+        class="input-container">
+        <label :for="field">{{ field }}</label>
+        <input
+          :id="field"
+          v-model="json.influencer.products[0][field]"
+          type="text">
+      </div>
+    </form>
     <pre>
       {{ json }}
     </pre>
@@ -20,15 +34,38 @@ export default {
 
   data() {
     return {
-      json: '',
+      json: JSON.parse(JSON.stringify(this.content)),
     };
   },
 
   methods: {
     save() {
-      return this.json;
+      const content = JSON.stringify(this.json, null, 2);
+      const a = document.createElement('a');
+      const file = new Blob([content], { type: 'application/json' });
+
+      a.href = URL.createObjectURL(file);
+      a.download = `${this.$route.params.gender}.${this.$route.params.franchise}.json`;
+      a.click();
     },
   },
 };
 </script>
+
+<style lang="scss" scoped>
+  .form {
+    max-width: 500px;
+    margin: 0 auto;
+
+    input {
+      width: 100%;
+      margin-bottom: 1rem;
+    }
+
+    label {
+      font-size: 18px;
+      display: block;
+    }
+  }
+</style>
 
